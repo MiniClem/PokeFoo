53f272ae58de3be2d459cf6e4c8f852a541ce222
Clem <cmonesti@gmail.com>
2020-08-25 00:22:03 +0200

    Added Loading state from sample

--- app/src/main/java/io/github/pokefoo/accueil/MainActivity.kt
+++ app/src/main/java/io/github/pokefoo/accueil/MainActivity.kt
@@ -5,37 +5,44 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import io.github.pokefoo.ExampleLoadStateAdapter
 import io.github.pokefoo.databinding.ActivityMainBinding
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    private lateinit var layout: ActivityMainBinding
-    private val model: MainActivityVM by viewModels()
+	private lateinit var layout: ActivityMainBinding
+	private val model: MainActivityVM by viewModels()
+	private lateinit var pokemonAdapter: PokemonAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        layout = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(layout.root)
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		layout = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(layout.root)
 
-        val pokemonAdapter = PokemonAdapter()
-        layout.mainRecycler.apply {
-            layoutManager =
-                GridLayoutManager(this@MainActivity, 4, GridLayoutManager.VERTICAL, false).apply {
-                    layoutParams
-                }
-            setHasFixedSize(true)
-            adapter = pokemonAdapter
-        }
-    }
+		pokemonAdapter = PokemonAdapter()
+		layout.mainRecycler.apply {
+			layoutManager =
+				GridLayoutManager(this@MainActivity, 4, GridLayoutManager.VERTICAL, false).apply {
+					layoutParams
+				}
+			setHasFixedSize(true)
+			adapter = pokemonAdapter.withLoadStateFooter(
+				footer = ExampleLoadStateAdapter()
+			)
+		}
+	}
 
-    override fun onStart() {
-        super.onStart()
-        lifecycleScope.launch {
-            model.pokemons.collectLatest {
-                (layout.mainRecycler.adapter as PokemonAdapter).submitData(it)
-            }
-        }
-    }
+	override fun onStart()
+	{
+		super.onStart()
+		lifecycleScope.launch {
+			model.pokemons.collectLatest {
+				pokemonAdapter.submitData(it)
+			}
+		}
+	}
 }
